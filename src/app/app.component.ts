50c2c4a1ad4e7671ee6ba37c251dbfdc5295b4cd
vibhitoshsingh <vibhitosh12@gmail.com>
2017-10-09 18:17:51 +0530

    till video 2.38

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css'],
+  styles:[`
+  h3{
+    color:dodgerblue;
+  }
+  `]
 })
 export class AppComponent {
-  title = 'app';
+  
+  // title = 'Does this Change ??';
+
+  //For Assingnment-1
+  // username='';
+
+// For Assingnment-2
+//   showSecret=false;
+//   log=[];
+//   onToggleDetails(){
+//     this.showSecret=!this.showSecret;
+//     //this.log.push(this.log.length +1);
+//      this.log.push(new Date ());
+//   }
 }
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //selector: '[app-servers]',
+  //selector: '.app-servers',
+ selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  //  template:`
+  //  <app-server></app-server>
+  //  <app-server></app-server>
+  //  `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+allowNewServer=false;
+serverCreationStatus='No server was created!';
+serverName='Testserver';
+serverCreated=false;
+servers=['Testserver','Testserver 2'];
+  constructor() {
+    setTimeout( () => {
+      this.allowNewServer=true;
+    },2000);
+   }
+
+  ngOnInit() {
+  }
+  onCreateServer(){
+this.serverCreated=true;
+this.servers.push(this.serverName);
+    this.serverCreationStatus='server was created! Name is' + this.serverName;
+  }
+  onUpdateServerName(event:Event){
+//console.log(event);
+this.serverName=(<HTMLInputElement>event.target).value;
+  }
+
+}
